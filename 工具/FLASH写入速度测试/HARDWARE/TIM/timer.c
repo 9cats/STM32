b302f6a20d1e402b310bf251eb485d57fef40307
Rainnn <123337671@qq.com>
2020-12-29 21:12:11 +0800

    测试完毕，写入频率为100KHz

--- 工具/FLASH写入速度测试/HARDWARE/TIM/timer.c
+++ 工具/FLASH写入速度测试/HARDWARE/TIM/timer.c
@@ -1,10 +1,11 @@
 #include "timer.h"
+#include "lcd.h"
 /********************************************************************
 ** 作者: 9cats
 ** 创建时间: 2020-11-24 20:10
 ** 适用环境: ALIENTEK STM32F407开发板
 ** 作用: 配置定时器和定时器中断服务函数
-** 资源: TIM3、TIM3_CH2 & GPIOA_PIN7
+** 资源: TIM3
 ** 备注: 
 *********************************************************************/
 //arr：自动重装值
@@ -29,62 +30,19 @@ void TIM3_Int_Init(u16 arr, u16 psc)
 	TIM_Cmd(TIM3, ENABLE);					   //使能定时器3
 
 	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;				 //定时器3中断
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02; //抢占优先级1
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03;		 //子优先级3
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00; //抢占优先级1
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;		 //子优先级3
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 }
 
-void TIM3_Int_Init2(u16 arr, u16 psc)
-{
-	GPIO_InitTypeDef GPIO_InitStructure;
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
-	TIM_ICInitTypeDef TIM3_ICInitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);  ///使能TIM3时钟
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //使能PORTA时钟
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;		   //GPIOA0
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;	   //复用功能
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz; //速度100MHz
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;	   //推挽复用输出
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;	   //下拉
-	GPIO_Init(GPIOA, &GPIO_InitStructure);			   //初始化PA0
-
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_TIM3); //复用
-
-	TIM_TimeBaseInitStructure.TIM_Period = arr;						//自动重装载值
-	TIM_TimeBaseInitStructure.TIM_Prescaler = psc;					//定时器分频
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; //向上计数模式
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStructure); //初始化TIM3
-
-	//初始化TIM3输入捕获参数
-	TIM3_ICInitStructure.TIM_Channel = TIM_Channel_2;				 //CC1S=01 	选择输入端 IC1映射到TI1上
-	TIM3_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;	 //上升沿捕获
-	TIM3_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI; //映射到TI1上
-	TIM3_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;			 //配置输入分频,不分频
-	TIM3_ICInitStructure.TIM_ICFilter = 0x00;						 //IC1F=0000 配置输入滤波器 不滤波
-	TIM_ICInit(TIM3, &TIM3_ICInitStructure);
-
-	TIM_ITConfig(TIM3, TIM_IT_Update | TIM_IT_CC2, ENABLE); //允许定时器3更新中断 ,允许CC1IE捕获中断
-	TIM_Cmd(TIM3, ENABLE);									//使能定时器3
-
-	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;				 //定时器3中断
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x02; //抢占优先级1
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x03;		 //子优先级3
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-}
-
+extern u32 count;
 /* 定时器3中断服务函数 */
 void TIM3_IRQHandler(void)
 {
-	if (TIM_GetITStatus(TIM3, TIM_IT_Update) || TIM_GetITStatus(TIM3, TIM_IT_CC2)) //溢出中断
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update)) //溢出中断
 	{
+		count++;
 	}
-
-	TIM_ClearITPendingBit(TIM3, TIM_IT_CC2 | TIM_IT_Update); //清除中断标志位
+	TIM_ClearITPendingBit(TIM3, TIM_IT_Update); //清除中断标志位
 }
--- 工具/FLASH写入速度测试/USER/main.c
+++ 工具/FLASH写入速度测试/USER/main.c
@@ -2,20 +2,57 @@
 #include "lcd.h"
 #include "usart.h"
 #include "delay.h"
+#include "timer.h"
+
+u32 count = 0;
 
 int main(void)
 {
-	u8 datatemp[20];
+	FLASH_Status status = FLASH_COMPLETE;
+	u32 addrx = 0x0800c004;
+	u32 WriteAddr = 0x0800c004;
+	u32 endaddr = 0x080FFFFF + 1;
 
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
 	delay_init(168);   //延时初始化
 	uart_init(115200); //串口初始化波特率为115200
 	LCD_Init();		   //初始化LCD FSMC接口
+	TIM3_Int_Init(10000 - 1, 84 - 1);//溢出频率为100
 
-	LCD_ShowString(30, 170, 200, 16, 16, "Start Read FLASH.... ");
-	STMFLASH_Read(0X0800C004, (u32 *)datatemp, 20);
-	LCD_ShowString(30, 170, 200, 16, 16, "The Data Readed Is:  "); //提示传送完成
-	LCD_ShowString(30, 190, 200, 16, 16, datatemp);
+	if (addrx < STM32_FLASH_BASE || addrx % 4)
+		return 0;					 //非法地址
+	FLASH_Unlock();				 //解锁
+	FLASH_DataCacheCmd(DISABLE); //FLASH擦除期间,必须禁止数据缓存
+
+	if (addrx < 0X1FFF0000)				  //只有主存储区,才需要执行擦除操作!!
+	{
+		while (addrx < endaddr) //扫清一切障碍.(对非FFFFFFFF的地方,先擦除)
+		{
+			if (STMFLASH_ReadWord(addrx) != 0XFFFFFFFF) //有非0XFFFFFFFF的地方,要擦除这个扇区
+			{
+				status = FLASH_EraseSector(STMFLASH_GetFlashSector(addrx), VoltageRange_3); //VCC=2.7~3.6V之间!!
+				if (status != FLASH_COMPLETE)
+					break; //发生错误了
+			}
+			else
+				addrx += 4;
+		}
+	}
+	if (status == FLASH_COMPLETE)
+	{
+		while (WriteAddr < endaddr) //写数据
+		{
+			if (FLASH_ProgramWord(WriteAddr, 0) != FLASH_COMPLETE) //写入数据
+			{
+				break; //写入异常
+			}
+			WriteAddr += 4;
+		}
+		FLASH_DataCacheCmd(ENABLE); //FLASH擦除结束,开启数据缓存
+		FLASH_Lock();				//上锁
+	}
+
+	LCD_ShowNum(30,30,count,5,16);
 	while (1)
 	{
 	}
