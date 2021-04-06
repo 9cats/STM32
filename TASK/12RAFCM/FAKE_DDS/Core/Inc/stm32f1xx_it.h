57302e9b32eaea9c4e4512ed6e4f44e266481ef3
HuangYi <123337671@qq.com>
2021-04-06 22:56:49 +0800

    总结：使用DAC TIM DMA USART EXTI，最后没能应用DDS的原理，最后决定更换至F4

--- TASK/12RAFCM/FAKE_DDS/Core/Inc/stm32f1xx_it.h
+++ TASK/12RAFCM/FAKE_DDS/Core/Inc/stm32f1xx_it.h
@@ -58,7 +58,6 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM2_IRQHandler(void);
-void USART1_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 void DMA2_Channel3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
